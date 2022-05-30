bc96dbbb5627dff4d87c0923b32a8906a3b805d6
YongQi Jiao <904771877@qq.com>
2022-05-30 23:03:15 +0800

    HelloWorld实现

--- Flutter/lib/main.dart
+++ Flutter/lib/main.dart
@@ -1,6 +1,21 @@
 
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(
+//     const Center(
+//       child: Text(
+//         "hello World",ww
+//         textDirection: TextDirection.ltr,
+//         style: TextStyle(
+//           fontSize: 30,
+//           color: Colors.orange
+//         ),
+//       ),
+//     )
+//   );
+// }
+
 void main() {
   runApp(MyApp());
 }
@@ -9,56 +24,36 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Scaffold (
+      title:"Hello World",
+      home: Scaffold(
         appBar: AppBar(
-          title: Text("CODERWHY"),
+          title: const Text('我是标题'),
         ),
-        body: HomeContent(),
+        body: ContentWidget(),
+        // appBar: AppBar(
+        //   title: const Text("Example title"),
+        // ),
       ),
     );
   }
 }
 
-class HomeContent extends StatelessWidget {
+class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: EdgeInsets.all(10),
-        child:ListView(
-          children: [
-            ProductItem("Apple1","Macbook Product1","http://www.baidu.com/img/bdlogo.png"),
-            ProductItem("Apple2","Macbook Product2","http://rongcloud-web.qiniudn.com/docs_demo_rongcloud_logo.png"),
-            ProductItem("Apple3","Macbook Product3","http://www.baidu.com/img/bdlogo.png"),
-          ],
-        )
-    );
+      return const Center(
+        child: Text(
+          "hello World",
+          textDirection: TextDirection.ltr,
+          style: TextStyle(
+              fontSize: 30,
+              color: Colors.orange
+          ),
+        ),
+      );
   }
 }
 
-class ProductItem extends StatelessWidget {
-  final String title;
-  final String subtitle;
-  final String imgurl;  
+void test(String name) {
 
-  ProductItem(this.title,this.subtitle,this.imgurl);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(8),
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 3
-        )
-      ),
-      child: Column(
-        children: [
-          Text(this.title,style: TextStyle(fontSize: 24)),
-          Text(this.subtitle,style: TextStyle(fontSize: 18)),
-          SizedBox(height: 10),
-          Image.network(this.imgurl)
-        ],
-      ),
-    );
-  }
 }
